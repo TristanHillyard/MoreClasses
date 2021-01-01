f7eba6a4d8491b67f1e3fa7b7fb88ab11f4c693f
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-01 11:40:02 -0700

    Add project files.

---
+++ MoreClasses/Div.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreClasses
+{
+    public class Div
+    {
+        public void Division(int num)
+        {
+            int result;
+            result = num / 2;
+            Console.WriteLine(result);
+        }
+        public void getValue(out int x)
+        {
+            int temp = 5;
+            x = temp;
+        }
+        
+    }
+}
---
+++ MoreClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoreClasses
+{
+    class Program
+    {
+        static int PlusMeth(int x, int y)
+        {
+            return x + y;
+        }
+        static double PlusMeth(double x, double y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            Div div = new Div();
+            int result;
+            Console.WriteLine("Enter any number.");
+            result = Convert.ToInt32(Console.ReadLine());
+            div.Division(result);
+
+            int a = 100;
+            div.getValue(out a);
+            Console.WriteLine(a);
+
+            int myNum = PlusMeth(8, 5);
+            double myNum1 = PlusMeth(2.3, 3.4);
+            Console.WriteLine(myNum);
+            Console.WriteLine(myNum1);
+        }
+    }
+}
